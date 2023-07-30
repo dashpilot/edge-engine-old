1a09e23b3eb3c4ab3db9333ab4ed422215730568
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-07-31 01:21:24 +0200

    Added static folder

--- api/render.js
+++ api/render.js
@@ -4,7 +4,7 @@ import template from "./../modules/index.mjs";
 
 var data = {
   pretitle: "Yes it's...",
-  title: "Edge Engine",
+  title: "Vertigo",
   features: [
     "Template files are compiled into native ES Modules and in-memory cached",
     "Supports Edge, SSR, SSG and CSR",
